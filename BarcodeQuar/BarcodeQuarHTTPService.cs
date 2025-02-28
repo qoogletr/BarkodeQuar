207089ce2c675bc6bb37217ac312083f47c6a6a3
qoogletr <109731179+qoogletr@users.noreply.github.com>
2025-02-28 13:37:01 +0300

    Add files via upload

---
+++ BarcodeQuar/BarcodeQuarHTTPService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace QuarBarcodeApp
+{
+    public class BarcodeQuarHTTPService
+    {
+        private readonly string _host;
+        private readonly int _port;
+        private HttpListener _listener;
+        private Thread _listenerThread;
+
+        public BarcodeQuarHTTPService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Start()
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add($"http://{_host}:{_port}/");
+            _listener.Start();
+            _listenerThread = new Thread(Listen);
+            _listenerThread.Start();
+            Console.WriteLine($"HTTP Servisi {_host}:{_port} adresinde başlatıldı.");
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listenerThread.Join();
+            Console.WriteLine("HTTP Servisi durduruldu.");
+        }
+
+        private void Listen()
+        {
+            while (_listener.IsListening)
+            {
+                try
+                {
+                    var context = _listener.GetContext();
+                    var response = context.Response;
+                    string responseString = "<HTML><BODY> Merhaba, dünya! </BODY></HTML>";
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                    response.ContentLength64 = buffer.Length;
+                    using (var output = response.OutputStream)
+                    {
+                        output.Write(buffer, 0, buffer.Length);
+                    }
+                }
+                catch (HttpListenerException) { }
+                catch (InvalidOperationException) { }
+            }
+        }
+    }
+}
---
+++ BarcodeQuar/BarcodeQuarSockService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace QuarBarcodeApp
+{
+    public class BarcodeQuarSockService
+    {
+        private readonly string _host;
+        private readonly int _port;
+        private TcpListener _listener;
+        private Thread _listenerThread;
+
+        public BarcodeQuarSockService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Start()
+        {
+            _listener = new TcpListener(IPAddress.Parse(_host), _port);
+            _listener.Start();
+            _listenerThread = new Thread(Listen);
+            _listenerThread.Start();
+            Console.WriteLine($"SOCKS Servisi {_host}:{_port} adresinde başlatıldı.");
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listenerThread.Join();
+            Console.WriteLine("SOCKS Servisi durduruldu.");
+        }
+
+        private void Listen()
+        {
+            while (true)
+            {
+                try
+                {
+                    var client = _listener.AcceptTcpClient();
+                    var stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string request = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"Gelen mesaj: {request}");
+                    byte[] response = System.Text.Encoding.UTF8.GetBytes("Merhaba, dünya!");
+                    stream.Write(response, 0, response.Length);
+                    client.Close();
+                }
+                catch (SocketException) { }
+            }
+        }
+    }
+}
